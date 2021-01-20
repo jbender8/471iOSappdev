25dceef79639dcecf8198f5c853dfc7ef45a3896
Jessica Bender <jessicabender18@gmail.com>
2021-01-19 20:27:38 -0600

    declarations part 3

---
+++ Week_2_Notes/Declarations3.playground/Contents.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+// 3.3
+
+let currencySymbol : Character = "$"
+
+let ten : Int32 = 10
+var itemCount : UInt = 0
+
+let length : Float = 100.0
+var totalAmount : Double = 0
+
+//3.4
+
+var total : Int
+var velocity : Double
+var statusMessage : String
+var isComplete : Bool
+
+total = 100
+velocity = 30.5
+statusMessage = "Success"
+isComplete = false
+
+//3.5
+
+let a = 3, b = 5
+let result = "\(a) times \(b) is \(a * b)"
+
+
+
