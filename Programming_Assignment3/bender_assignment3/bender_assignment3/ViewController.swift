c616f92e05de1d86b0c64e865a0d8bbdb38b8f1d
Jessica Bender <jessicabender18@gmail.com>
2021-01-26 19:54:04 -0600

    added buttons to swift file

--- Programming_Assignment3/bender_assignment3/bender_assignment3/ViewController.swift
+++ Programming_Assignment3/bender_assignment3/bender_assignment3/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var num = ""
+    var total = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func one(_ sender: Any) {
+    }
+    
+    @IBAction func two(_ sender: Any) {
+    }
+    
+    @IBAction func three(_ sender: Any) {
+    }
+    
+    
+    @IBAction func four(_ sender: Any) {
+    }
+    
+    @IBAction func five(_ sender: Any) {
+    }
+    
+    @IBAction func six(_ sender: Any) {
+    }
+    
+    @IBAction func seven(_ sender: Any) {
+    }
+    
+    @IBAction func eight(_ sender: Any) {
+    }
+    
+    @IBAction func nine(_ sender: Any) {
+    }
+    
+    @IBAction func zero(_ sender: Any) {
+    }
+    
+    @IBAction func add(_ sender: Any) {
+    }
+    
+    @IBAction func equal(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var output: UILabel!
+    
+    
 }
+
 
