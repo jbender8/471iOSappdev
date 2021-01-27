b5d0967e7130d29c2c9dac61d726d984c705264f
Jessica Bender <jessicabender18@gmail.com>
2021-01-26 18:04:26 -0600

    set up assignment 3

---
+++ Programming_Assignment3/bender_assignment3/bender_assignment3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bender_assignment3
+//
+//  Created by Jessica Bender on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
