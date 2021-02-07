ddf9d9a7ccb429af79734dcf237c9d789191094b
Jessica Bender <jessicabender18@gmail.com>
2021-02-06 23:25:36 -0600

    finished

--- Programming_Assignment5/jbender_assignment5/jbender_assignment5/ViewController.swift
+++ Programming_Assignment5/jbender_assignment5/jbender_assignment5/ViewController.swift
@@ -23,7 +23,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var LIST: UILabel!
     
     
+    @IBAction func list(_ sender: Any) {
+        items = []
+        LIST.text = "No Items"
+    }
     
+    @IBAction func item(_ sender: Any) {
+        Des.text = ""
+        Descrip.text = ""
+        
+        
+    }
+    
+    @IBAction func add(_ sender: Any) {
+        let alert = UIAlertController(title: "Invalid Input", message: "Descptions or Quanity invalid", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "ok", style: .default, handler: {action in switch action.style{ case .default: print("default") case .cancel: print("cancel") case .destructive: print("destructive") }}))
+        
+        if (Int(Descrip.text!) ?? 0 < 1) || (Des.text == "") {
+            self.present(alert, animated: true, completion: nil)
+        }
+        else{
+            items.append((Des.text!, Int(Descrip.text!)!))
+            LIST.text = ""
+            LIST.numberOfLines = 0
+            for i in items{
+                LIST.text! += String(i.1) + "x" + i.0 + "\n"
+            }
+            LIST.sizeToFit()
+        }
+        Des.text = ""
+        Descrip.text = ""
+    }
     
     
     
