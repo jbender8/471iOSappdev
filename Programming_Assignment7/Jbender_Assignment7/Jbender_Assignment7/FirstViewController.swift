c496afc9e55d93cd069310b663d681df8756a190
Jessica Bender <jessicabender18@gmail.com>
2021-02-22 18:19:46 -0600

    configre fav chan design

---
+++ Programming_Assignment7/Jbender_Assignment7/Jbender_Assignment7/FirstViewController.swift
@@ -0,0 +1,56 @@
+//
+//  FirstViewController.swift
+//  Jbender_Assignment7
+//
+//  Created by Jessica Bender on 3/11/20.
+//  Copyright © 2020 Jessica Bender. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+      override func viewDidLoad() {
+            super.viewDidLoad()
+
+            channelStepOutlet.autorepeat = true
+            channelStepOutlet.wraps = true
+            channelStepOutlet.maximumValue = 99
+            channelStepOutlet.minimumValue = 1
+            // Do any additional setup after loading the view.
+        }
+        
+        @IBOutlet weak var buttonsSeg: UISegmentedControl!
+        @IBOutlet weak var labelTextField: UITextField!
+        @IBOutlet weak var channelStepOutlet: UIStepper!
+        @IBOutlet weak var channelOutlet: UILabel!
+        
+        @IBAction func channelStep(_ sender: UIStepper) {
+            channelOutlet.text = Int(sender.value).description
+        }
+        
+        @IBAction func cancel(_ sender: UIButton) {
+            labelTextField.text = ""
+            channelStepOutlet.value = 1
+            channelOutlet.text = "1"
+        }
+        
+        @IBAction func save(_ sender: UIButton) {
+            if (labelTextField.text!.count < 1 || labelTextField.text!.count > 4) {
+                let alert = UIAlertController(title: "Invalid Label Length", message: "Label must be between 1 and 4 characters long", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { ACTION in
+                }))
+                self.present(alert, animated: true)
+            }
+            else {
+                if channelOutlet.text!.count == 2 {
+                    LabChannels[buttonsSeg.selectedSegmentIndex] = channelOutlet.text!
+                }
+                else {
+                    LabChannels[buttonsSeg.selectedSegmentIndex] = "0" + channelOutlet.text!
+                }
+                LabChannels[buttonsSeg.selectedSegmentIndex] = labelTextField.text!
+                NotificationCenter.default.post(name: .Favsave, object: nil)
+            }
+        }
+    }
---
+++ Programming_Assignment7/Jbender_Assignment7/Jbender_Assignment7Tests/Jbender_Assignment7Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Jbender_Assignment7Tests.swift
+//  Jbender_Assignment7Tests
+//
+//  Created by Jessica Bender on 3/11/20.
+//  Copyright © 2020 Jessica Bender. All rights reserved.
+//
+
+import XCTest
+@testable import Jbender_Assignment7
+
+class Jbender_Assignment7Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
